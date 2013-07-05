d6dac7603e1c61d449732f810fd60f74882c9bbc
Anthony Doan <anthonydoan@Macintosh.local>
2013-07-05 07:38:55 -0700

    Initial Commit

---
+++ Books/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Books
+//
+//  Created by Anthony Doan on 7/5/13.
+//  Copyright (c) 2013 Mobile Tuts. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
